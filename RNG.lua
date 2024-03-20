f62e2400b7fb330ea9962267d2daba1209501ab0
9001-Solutions <9001.sols@gmail.com>
2024-03-20 13:27:57 -0400

    Crafting Bangles Fix

--- RNG.lua
+++ RNG.lua
@@ -236,7 +236,7 @@ profile.HandleDefault = function()
     end
 
     -- If health isn't max throw on regen items
-    if (player.HPP < 99 and isDaytime() and aux ~= "Gathering") then
+    if (player.HPP < 99 and isDaytime() and aux ~= "Gathering" and aux ~= "Crafting") then
         gFunc.Equip("hands", "Garden Bangles");
     end
 
